bf69ac8d967d2f57fba1c1ba6226f2e8a1963f95
MT-Cobbler <mtschoeman123@gmail.com>
2020-09-29 12:19:09 +0200

    beautified the script.js

--- Ass_5/A5-jQuery+AJAX1/script.js
+++ Ass_5/A5-jQuery+AJAX1/script.js
@@ -18,7 +18,6 @@ $(document).ready(function() {
         }
     })
     const getUsers = url => {
-
         return users = new Promise((res, rej) => {
             $.getJSON(url).done(data => {
                 res(data);
@@ -27,7 +26,6 @@ $(document).ready(function() {
     }
 
     const createUserCard = () => {
-
         let card = "<div class='card mb-3'><div class='row no-gutters'><div class='col-md-4'><img src='bg.png' class='card-img' alt=''...'></div><div class='col-md-8'><div class='card-body'><h5 class='card-title'></h5><p class='card-text'></p></div></div></div></div>";
         return card;
     }
