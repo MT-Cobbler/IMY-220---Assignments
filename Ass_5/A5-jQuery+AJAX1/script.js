62156a26df9bf7ccc08910fb1db7414639a1e75e
MT-Cobbler <mtschoeman123@gmail.com>
2020-09-29 13:31:29 +0200

    Final script

--- Ass_5/A5-jQuery+AJAX1/script.js
+++ Ass_5/A5-jQuery+AJAX1/script.js
@@ -1,20 +1,24 @@
 $(document).ready(function() {
+    $("div.loading mb-3").remove();
     $(window).on("scroll", async function() {
         if ($(window).scrollTop() >= $(document).height() - $(window).height()) {
             //Add something at the end of the page
             console.log("You have reached the bottom of the page");
             let url = "users.json";
             var duration = 1000;
-            sleep(duration)
             const response = await getUsers(url)
             const processResponse = await sleep(duration);
             let objLength = response.length;
+            let copyLoading = $("div.loading mb-3");
+
             for (let i = 0; i < objLength; i++) {
-                $(createUserCard()).insertBefore(".loading")
+                // $(createUserCard()).insertBefore(".loading")
+                $(".loading").remove();
+                $("#userList").append(createUserCard)
                 $(".card-title").last().html(response[i].name + " " + response[i].surname);
                 $(".card-text").last().html(response[i].quote);
             }
-
+            $("#userList").append("<div class = 'loading mb-3' ><img src = 'loading.gif' / ></div>");
         }
     })
     const getUsers = url => {
