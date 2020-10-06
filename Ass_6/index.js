a053c3841df5f0d0e9fef4625bdcb3070000c07b
MT-Cobbler <mtschoeman123@gmail.com>
2020-10-06 19:01:55 +0200

    Assgignment 6

--- Ass_6/index.js
+++ Ass_6/index.js
@@ -1,13 +1,29 @@
-const express = require('express')
+//create a file using node
+const fs = require('fs');
+const filename = "messageLog.txt";
+fs.writeFile(filename, 'Messages' + '\r\n', (err) => {
+    if (err) throw err;
 
+})
+
+const express = require('express');
 const app = express();
-app.use(express.static('public'));
+const server = app.listen(3000, () => {
+    console.log('Listening at localhost:3000');
+});
+const io = require('socket.io').listen(server);
 
 app.get('/', (req, res) => {
-    res.send('hello world');
-
+    res.sendFile(__dirname + '/public/');
 });
 
-app.listen(3000, () => {
-    console.log("Listening on localhost:1337");
-})
+io.on('connection', (socket) => {
+    socket.on('message', (msg, name) => {
+        console.log('Name: ' + name + ', Message: ' + msg);
+        let line = 'Name: ' + name + ', Message: ' + msg + '\r\n';
+        fs.appendFile(filename, line, (err) => {
+            if (err) throw err;
+
+        })
+    })
+});
